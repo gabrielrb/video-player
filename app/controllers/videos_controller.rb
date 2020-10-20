cd790df0b6dfa76328c5fc48a4be4d64da29456c
gabriel belo <gabrielrbelo@gmail.com>
2020-10-20 11:46:43 -0300

    show route, action and view created

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,16 @@
 class VideosController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_video, only: :show
 
   def index
     @videos = Video.all
+  end
+
+  def show; end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :videos, only: :index
+  resources :videos, only: [:index, :show]
 end
