bb7b3d504722d4e7ade618f8a7ce3ff024767206
gabriel belo <gabrielrbelo@gmail.com>
2020-10-20 15:15:28 -0300

    new and create actions and view created; all routes created;

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,10 +8,30 @@ class VideosController < ApplicationController
 
   def show; end
 
+  def new
+    @video = Video.new
+    authorize @video
+  end
+
+  def create
+    @video = Video.new(video_params)
+    @video.user = current_user
+    authorize @video
+    if @video.save
+      redirect_to video_path(@video), notice: 'Your video was successfully created'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_video
     @video = Video.find(params[:id])
     authorize @video
+  end
+
+  def video_params
+    params.require(:video).permit(:name, :url)
   end
 end
--- app/policies/video_policy.rb
+++ app/policies/video_policy.rb
@@ -12,4 +12,8 @@ class VideoPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def create?
+    true
+  end
 end
