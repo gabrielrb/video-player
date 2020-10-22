5cfe1c4b54e0dbb1a26b13e10700ee94722c2e4b
gabriel belo <gabrielrbelo@gmail.com>
2020-10-22 20:52:38 -0300

    video validations created

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,8 @@
 class Video < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :url, presence: true,
+                  format: { with: /\A.*(\.m3u8)\z/, message: "url must end with '.m3u8'" }
+
 end
