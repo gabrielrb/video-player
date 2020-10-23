2b3b678ecfe9e9696b2ceb06bb52c240c2b639fd
gabriel belo <gabrielrbelo@gmail.com>
2020-10-23 12:30:32 -0300

    video counter in progress

--- app/javascript/components/video_player.js
+++ app/javascript/components/video_player.js
@@ -1,9 +1,20 @@
-var player = videojs('user_video')
-var vhs = player.tech().vhs;
+const player = videojs('user-video')
 
 function initPlayer() {
   return player
 }
 
 
-export { initPlayer }
+let counter = document.getElementById("video-views");
+let video = document.getElementById("user-video");
+
+
+let amount = 0
+function viewCounter() {
+  video.addEventListener("play", function() {
+    amount += 1
+    counter.insertAdjacentHTML("beforeend", amount);
+}, true);
+}
+
+export { initPlayer, viewCounter }
