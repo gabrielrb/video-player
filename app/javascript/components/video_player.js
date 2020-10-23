b0ec0efb349d50d0785ab4f16fca75a0efb75519
gabriel belo <gabrielrbelo@gmail.com>
2020-10-23 15:54:36 -0300

    video counter in progress

--- app/javascript/components/video_player.js
+++ app/javascript/components/video_player.js
@@ -1,19 +1,27 @@
 const player = videojs('user-video')
+const video = document.getElementById("user-video");
+let counter = document.getElementById("video-views");
 
 function initPlayer() {
   return player
 }
 
 
-let counter = document.getElementById("video-views");
-let video = document.getElementById("user-video");
+function eraseViews(counter) {
+  if (counter.firstChild) {
+    counter.removeChild(counter.firstChild)
+  }
+}
 
 
 let amount = 0
+
+
 function viewCounter() {
   video.addEventListener("play", function() {
     amount += 1
-    counter.insertAdjacentHTML("beforeend", amount);
+    eraseViews(counter);
+    counter.innerHTML = amount;
 }, true);
 }
 
