ad35cb1dc5f767cd0783d49eb455c3134a676787
gabriel belo <gabrielrbelo@gmail.com>
2020-10-25 00:07:04 -0300

    video_views.yml created; index test created and passing;

--- test/system/videos_test.rb
+++ test/system/videos_test.rb
@@ -3,7 +3,8 @@ require "application_system_test_case"
 class VideosTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
-
-    assert_selector "h1", text: "All videos"
+    # save_and_open_screenshot
+    assert_selector "h2", text: "All videos"
+    assert_selector ".card-product", count: Video.count
   end
 end
